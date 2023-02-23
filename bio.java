f0d9b79ffd384729b826d9d2ef6f704ef0df21ab
Jobayer joyaddar <66490138+Jobayer75@users.noreply.github.com>
2023-02-24 06:58:34 +0800

    Create bio.java

---
+++ bio.java
@@ -0,0 +1,19 @@
+package basicjava;
+
+public class BioData {
+    public static void main(String[] args)
+    {
+    //Normal process
+    System.out.println("Badhon");
+    System.out.println("01164009242");
+    System.out.println("kushtia");
+   
+    System.out.print("Jobayer\n01164009242\nKushtia\n");
+   //escape sequence
+    System.out.print("kushtia \tDaulatpur\n\n");
+       //use tab 
+    }
+ }
+  
+   
+    
