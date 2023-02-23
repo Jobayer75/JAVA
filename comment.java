8090e29fb0b5e1b51417c4892b5ce020eb90884a
Jobayer joyaddar <66490138+Jobayer75@users.noreply.github.com>
2023-02-24 06:59:18 +0800

    Create comment.java

---
+++ comment.java
@@ -0,0 +1,21 @@
+
+package basicjava;
+
+public class CommentDemo {
+    public static void main(String[] args){
+        
+        
+    System.out.println("Jobayer");
+    System.out.println("23 years old");
+    
+    //here print my name      single cm
+    
+    /*
+    Jobayer
+    biodata of program        multiple cm
+    15/4/2020
+    */
+        
+}
+    
+     }
