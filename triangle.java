24d1b6dd24a4b115b59e0b90671cc718179acfb3
Jobayer joyaddar <66490138+Jobayer75@users.noreply.github.com>
2023-02-24 06:57:43 +0800

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,24 @@
+public class Triangle {
+	
+	double xCoordinate;
+	double yCoordinate;
+	
+	//Getter
+	public double getXCoordinate(){
+		return xCoordinate;
+	}
+	
+	public double getYCoordinate() {
+		return xCoordinate;
+	}
+	
+	//Setter
+	public void setXCoordinate(double newXCoordinate) {
+		this.xCoordinate = newXCoordinate;
+	}
+	
+	public void setYCoordinate(double newYCoordinate) {
+		this.yCoordinate = newYCoordinate;
+	}
+
+}
